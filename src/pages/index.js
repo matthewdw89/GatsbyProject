5ac1505d10a146273eb76b05a698a5ff5b4f8ca2
Matt Waters <matthewdw89@gmail.com>
2018-12-17 16:17:50 -0500

    first commit

---
+++ src/pages/index.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Link } from 'gatsby'
+
+import Layout from '../components/layout'
+import SEO from '../components/seo'
+
+const IndexPage = () => (
+  <Layout>
+    <SEO title="Home" keywords={['Ophthalmology', 'eye doctor', 'glaucoma', 'vitreoretinal surgery']} />
+    <h1>Hi people</h1>
+    <p>Welcome to your new Gatsby site.</p>
+    <p>Now go build something great.</p>
+    <Link to="/contact/">Go to page 2</Link>
+  </Layout>
+)
+
+export default IndexPage
